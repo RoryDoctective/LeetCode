3018cdb7243c324e931b37353f478722a94dc75c
Rory <644828632@qq.com>
2023-03-28 14:11:08 +0800

    持续专项练习链表题目： 1. 优化了第20题解法. 2. 写了并完善了21题 3. 初步写了23题

--- leetcode得题/20.py
+++ leetcode得题/20.py
@@ -1,8 +1,13 @@
-# 链表， delete 一个
-# 双指针，end condition很恶心
+'''
+链表， delete 一个 node
+方法：
+    1. 双指针，end condition很恶心
+    2. 数长度
+    3. 栈/queue 【最不好】
+'''
 
 
-# 双指针
+# 双指针 -》 自己写的很麻烦
 # Definition for singly-linked list.
 # class ListNode:
 #     def __init__(self, val=0, next=None):
@@ -37,3 +42,61 @@ class Solution:
                 return head.next
 
         return head
+
+
+# 双指针，化简版本
+class Solution:
+    def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
+        # 双指针，简单一点
+        dummy = ListNode(-1,head)
+        p1 = dummy
+        p2 = head
+        for i in range(n):
+            p2 = p2.next
+        while p2:
+            p1 = p1.next
+            p2 = p2.next
+        p1.next = p1.next.next
+        return dummy.next
+
+
+# 数长度
+class Solution:
+    def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
+        # length
+        def getLength(head):
+            length = 0
+            while head:
+                length += 1
+                head = head.next
+            return length
+
+        # get length
+        length = getLength(head)
+        dummy = ListNode(-1, head)
+        p = dummy
+        # before_node = length - n
+        for _ in range(1, length - n + 1):  # [1 - length-n ]
+            p = p.next
+
+        p.next = p.next.next
+        return dummy.next
+
+
+# 用栈。
+# add all then pop n, the last is the point
+# ps: List all the node
+class Solution:
+    def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
+        # Stack/栈
+        stack = []
+        dummy = ListNode(-1, head)
+        p = dummy
+        while p:
+            stack.append(p)
+            p = p.next
+        for i in range(n):
+            stack.pop()
+        prev = stack[-1]
+        prev.next = prev.next.next
+        return dummy.next
